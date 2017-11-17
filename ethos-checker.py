d4651caf356437dbde34082a542d0f839b141cd9
Kooshini <kooshini@gmail.com>
2017-11-17 00:23:59 +0000

    Added more checks
    Added more checks for hash rate (config via the ENV var)

--- ethos-checker.py
+++ ethos-checker.py
@@ -9,6 +9,7 @@
 # SMTP_PORT
 # SMTP_TO
 # SMTP_FROM
+# EXP_HASH
 #################################
 import os, math, smtplib, socket, urllib.request, json
 ###### configure below ##########
@@ -21,6 +22,8 @@ smtpHost = os.environ['SMTP_HOST']
 smtpPort = os.environ['SMTP_PORT']
 mailRecipients = os.environ['SMTP_TO'] ## add more  ['email1@example.com', 'email2@example.com']
 mailSender = os.environ['SMTP_FROM'] ## your@email.com
+## hash checks
+expHash = int(os.environ['EXP_HASH']) ## e.g. 24 for 24MH/s etc
 ###### end configure ##########
 ###############################
 def lambda_handler(event, context):
@@ -31,13 +34,32 @@ def lambda_handler(event, context):
         alive_gpus = data['alive_gpus']
         total_gpus = data['total_gpus']
         total_hash = data['total_hash']
-    ## fire off an email if the space is above the required number
-    if alive_gpus < total_gpus:
+        avg_temp = data['avg_temp']
+    ## fire off an email if the triggers are met
+    if alive_gpus < 7:
         print("Error: The number of GPUs is %d/%d - total hash is %d" % (alive_gpus,total_gpus,total_hash))
         ## send email via the SMTP server
         server = smtplib.SMTP(smtpHost,smtpPort)
         server.starttls()
         server.login(smtpLogin, smtpPass)
         msg = "Subject: Urgent: The number of GPUs is %d/%d - total hash is %s" % (alive_gpus,total_gpus,total_hash)
+        server.sendmail(mailSender, mailRecipients, msg)
+        server.quit()
+    if total_hash < expHash:
+        print("Error: The hashrate has dropped - current hash %d (expecting %d)" % (total_hash,expHash))
+        ## send email via the SMTP server
+        server = smtplib.SMTP(smtpHost,smtpPort)
+        server.starttls()
+        server.login(smtpLogin, smtpPass)
+        msg = "Subject: Urgent: The hashrate has dropped - current hash %d (expecting %d)" % (total_hash,expHash)
+        server.sendmail(mailSender, mailRecipients, msg)
+        server.quit()
+    if avg_temp > 55:
+        print("Error: The average temp is %d" % (avg_temp))
+        ## send email via the SMTP server
+        server = smtplib.SMTP(smtpHost,smtpPort)
+        server.starttls()
+        server.login(smtpLogin, smtpPass)
+        msg = "Subject: Urgent: The temp is too high! Avg temp is %d" % (avg_temp)
         server.sendmail(mailSender, mailRecipients, msg)
         server.quit()
